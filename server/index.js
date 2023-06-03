c3fadc3cb300baed001c92bb0ce64ca5a306c2d2
chmourad <mourad.chabanechaouche@fegi.ummto.dz>
2023-06-03 22:17:57 +0200

    user router modifications

--- server/index.js
+++ server/index.js
@@ -1,13 +1,42 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require('body-parser');
+const handleLogin = require('./loginHandler'); // Chemin vers votre fichier de gestionnaire de connexion
+const handleRegister = require('./registerHandler'); // Chemin vers votre fichier de gestionnaire d'inscription
+const deleteHandler = require('./deleteHandler');
+const { isAuthenticated } = require('./middlewares'); // Inclure le middleware d'authentification approprié
+const updateHandler = require('./updateHandler');
+
+
+// Utilisez le middleware body-parser pour analyser le corps des requêtes
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const userRouter = require("./src/router/userRouter/userRouter");
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors);
 
 app.use("/user", userRouter);
+
+// Utilisez le middleware body-parser pour analyser le corps des requêtes
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//la fonction app.post() pour définir une route de connexion 
+app.post('/login', handleLogin);
+
+// Définition de la route d'inscription
+app.post('/register', handleRegister);
+
+// Route de suppression d'un utilisateur avec vérifications d'authentification et d'autorisation
+app.delete('/users/:id', isAuthenticated, deleteHandler);
+
+// Route de mise à jour d'un utilisateur
+app.put('/users/:id', updateHandler);
+
 app.listen(8080, () => {
   console.log("listening on port 8080");
 });
--- server/src/router/userRouter/userHandlers/handleDelete.js
+++ server/src/router/userRouter/userHandlers/handleDelete.js
@@ -1,5 +1,35 @@
-const handleDelete = (req, res) => {
+/*const handleDelete = (req, res) => {
   res.json({ res: true });
 };
 
-module.exports = handleDelete;
+module.exports = handleDelete;*/
+
+const deleteHandler = (req, res) => {
+  const userId = req.params.id;
+
+  // Vérification de l'authentification
+  if (!req.isAuthenticated()) {
+    return res.status(401).json({ error: 'Non authentifié' });
+  }
+
+  // Vérification de l'autorisation
+  if (req.user.id !== userId) {
+    return res.status(403).json({ error: 'Accès refusé' });
+  }
+
+  // Suppression de l'utilisateur dans la base de données
+  User.findByIdAndRemove(userId, (err, deletedUser) => {
+    if (err) {
+      return res.status(500).json({ error: 'Erreur lors de la suppression de l\'utilisateur' });
+    }
+
+    if (!deletedUser) {
+      return res.status(404).json({ error: 'Utilisateur non trouvé' });
+    }
+
+    res.status(200).json({ message: 'Utilisateur supprimé avec succès' });
+  });
+};
+
+module.exports = deleteHandler;
+
